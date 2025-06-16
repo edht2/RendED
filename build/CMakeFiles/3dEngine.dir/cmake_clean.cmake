7656449711d7632e899d57ffc638dcbe4541676a
Ed Haig-Thomas <ehaigthomas@gmail.com>
2025-06-16 08:51:25 +0100

    started adding cmake

---
+++ build/CMakeFiles/3dEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3dEngine"
+  "3dEngine.pdb"
+  "CMakeFiles/3dEngine.dir/tests/test1.cpp.o"
+  "CMakeFiles/3dEngine.dir/tests/test1.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3dEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
